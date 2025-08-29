534b4f42f4b8113a20d84981bf974026c838685f
PPPoint <1024879159@qq.com>
2025-08-29 16:09:28 +0800

    Add sub op cambricon

---
+++ src/infiniop/ops/sub/bang/sub_bang.h
@@ -0,0 +1,8 @@
+#ifndef __SUB_BANG_API_H__
+#define __SUB_BANG_API_H__
+
+#include "../../../elementwise/bang/elementwise_bang.h"
+
+ELEMENTWISE_DESCRIPTOR(sub, bang)
+
+#endif // __SUB_BANG_API_H__

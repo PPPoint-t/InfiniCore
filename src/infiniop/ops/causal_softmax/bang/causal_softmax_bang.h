222361f791b894c3da6d1f9b34dc721b3b56a777
PPPoint <1024879159@qq.com>
2025-09-01 16:12:16 +0800

    Add causal softmax op cambricon

---
+++ src/infiniop/ops/causal_softmax/bang/causal_softmax_bang.h
@@ -0,0 +1,8 @@
+#ifndef __CAUSAL_SOFTMAX_BANG_H__
+#define __CAUSAL_SOFTMAX_BANG_H__
+
+#include "../causal_softmax.h"
+
+DESCRIPTOR(bang)
+
+#endif
--- src/infiniop/ops/causal_softmax/bang/causal_softmax_cnnl.h
+++
@@ -1,8 +0,0 @@
-#ifndef __CAUSAL_SOFTMAX_CNNL_H__
-#define __CAUSAL_SOFTMAX_CNNL_H__
-
-#include "../causal_softmax.h"
-
-DESCRIPTOR(cnnl)
-
-#endif // __CAUSAL_SOFTMAX_CNNL_H__
